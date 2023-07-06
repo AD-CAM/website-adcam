19d22de8a74f5c023d53c9800e590c91f5a855d9
altervayne <79285331+Altervayne@users.noreply.github.com>
2023-07-07 01:31:39 +0200

    set basic body and html styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,27 @@
+'use client'
+
 import Image from 'next/image'
+import { makeStyles } from 'tss-react/mui'
+
+const useStyles = makeStyles()((theme) => {
+    return {
+        root: {
+            width: "100%",
+            margin: 0,
+            padding: 0,
+        }
+    }
+})
 
 
 export default function Home() {
+    const { classes } = useStyles()
+
     return (
-        <main>
-            test
-        </main>
+        <>
+            <main className={ classes.root }>
+                test
+            </main>
+        </>
     )
 }
