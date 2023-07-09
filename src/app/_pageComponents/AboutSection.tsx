e82d200779cbb08440ee7d987c08de18400c7158
altervayne <79285331+Altervayne@users.noreply.github.com>
2023-07-09 12:59:46 +0200

    prepared second section pageComponent

---
+++ src/app/_pageComponents/AboutSection.tsx
@@ -0,0 +1,26 @@
+'use client'
+/* Library Imports */
+import { makeStyles } from 'tss-react/mui'
+import { motion, Variants } from 'framer-motion'
+/* Components Imports */
+
+
+
+const useStyles = makeStyles()((theme) => {
+    return {
+        root: {
+        },
+    }
+})
+
+
+
+export default function AboutSection() {
+    const { classes } = useStyles()
+
+    return (      
+        <section className={ classes.root }>
+
+        </section>
+    )
+}
