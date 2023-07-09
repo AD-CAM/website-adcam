d31169f557d2a1f888747dcc08ceac487d8bdcd1
altervayne <79285331+Altervayne@users.noreply.github.com>
2023-07-10 01:40:28 +0200

    added shadowing to the section

--- src/app/_pageComponents/AboutSection.tsx
+++ src/app/_pageComponents/AboutSection.tsx
@@ -17,6 +17,8 @@ const useStyles = makeStyles()((theme) => {
             width: "100%",
 
             paddingTop: theme.spacing(10),
+
+            boxShadow: "0px 0px 10px 0px rgba(0, 0, 0, 0.37)",
         },
         subRoot: {
             maxWidth: "1450px",
