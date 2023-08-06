b1f93169231d2f133b7e16e2ea16e941d73e033a
altervayne <79285331+Altervayne@users.noreply.github.com>
2023-08-06 09:10:24 +0200

    created privacy policy page file

---
+++ src/app/politique_de_confidentialite/page.tsx
@@ -0,0 +1,22 @@
+/* Assets Imports */
+import './globals.css'
+/* Components Imports */
+import CallUs from '../_components/CallUs'
+import Header from '../_components/Header'
+import Footer from '../_components/Footer'
+/* Page Components Imports */
+
+
+
+export default function PrivacyPolicyPage() {
+    return (
+        <>
+            <Header activePage={ "home" } />
+            <CallUs />
+            <main>
+                
+            </main>
+            <Footer />
+        </>
+    )
+}
