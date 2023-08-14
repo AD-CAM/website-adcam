ed7f8204930e55b0b2c0622c98a1c27b0ac3186e
altervayne <79285331+Altervayne@users.noreply.github.com>
2023-08-14 10:13:02 +0200

    created fourth paragraph of legal notices

--- src/app/mentions-legales/_pageComponents/LegalNotice.tsx
+++ src/app/mentions-legales/_pageComponents/LegalNotice.tsx
@@ -119,9 +119,9 @@ export default function LegalNotices() {
                         
                         
                         <h3 className={ classes.textTitle }>{ `4. Limitations contractuelles sur les données techniques.` }</h3>
-                        <p className={ classes.textParagraph }>{ `` }</p>
+                        <p className={ classes.textParagraph }>{ `Le site utilise la technologie JavaScript.` }</p>
 
-                        <p className={ classes.textParagraph }>{ `` }</p>
+                        <p className={ classes.textParagraph }>{ `Le site Internet ne pourra être tenu responsable de dommages matériels liés à l’utilisation du site. De plus, l’utilisateur du site s’engage à accéder au site en utilisant un matériel récent, ne contenant pas de virus et avec un navigateur de dernière génération mis-à-jour` }</p>
                         
                         
                         
