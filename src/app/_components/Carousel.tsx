9d45f8da5119636b992442b9904944378146b425
altervayne <79285331+Altervayne@users.noreply.github.com>
2023-08-16 13:40:24 +0200

    fixed carousel display order and animated it

--- src/app/_components/Carousel.tsx
+++ src/app/_components/Carousel.tsx
@@ -47,7 +47,7 @@ const useStyles = makeStyles()((theme) => {
             
             width: "auto",
 
-            transition: "transform 0.3s ease-in-out",
+            transition: "all 0.3s ease-in-out",
         }
     }
 })
@@ -71,7 +71,7 @@ export default function Carousel({ children }: CarouselProps) {
         setActiveIndex((prevIndex) => (prevIndex - 1 + children.length) % children.length);
     }
 
-    function isEven() {
+    function isEven(): boolean {
         if(Math.floor(children.length / 2) - (children.length / 2) === 0) {
             return true
         } else {
@@ -79,19 +79,33 @@ export default function Carousel({ children }: CarouselProps) {
         }
     }
 
-    const offset = Math.floor((children.length - 2) / 2)
+    function calculateOpacity(index: number): number {
+        const distanceFromActive = Math.abs(activeIndex - index)
+        const maxOpacity = 1
+        const minOpacity = 0.1
+        
+        return Math.max(maxOpacity - distanceFromActive * 0.35, minOpacity)
+    }
+
+
+
+    const offset = Math.floor((children.length - 2) / 2) + 1
 
     return (      
         <div className={ classes.root}>
-            <button className={ classes.buttonLeft } onClick={ handleNext }>{ `<` }</button>
+            <button className={ classes.buttonLeft } onClick={ handlePrevious }>{ `<` }</button>
                 <div className={ classes.content }>
                     {
                         children.map((child, index) => {
                             return (
                                 <div    key={ index }
                                         className={ classes.slide }
-                                        style={ isEven()    ? { transform: `translateX(-50%) translateX(${(activeIndex - offset) * 350}px)` }
-                                                            : { transform: `translateX(${(activeIndex - offset - 1) * 350}px)` }}
+                                        style={ isEven()    ? { transform: `translateX(-50%) translateX(${(-activeIndex + offset) * 350}px)`,
+                                                                opacity: calculateOpacity(index)
+                                                            }
+                                                            : { transform: `translateX(${(-activeIndex + offset) * 350}px)`,
+                                                                opacity: calculateOpacity(index)
+                                                            }}
                                 >
                                     { child }
                                 </div>
@@ -99,7 +113,7 @@ export default function Carousel({ children }: CarouselProps) {
                         })
                     }
                 </div>
-            <button className={ classes.buttonRight } onClick={ handlePrevious }>{ `>` }</button>
+            <button className={ classes.buttonRight } onClick={ handleNext }>{ `>` }</button>
         </div> 
     )
 }
