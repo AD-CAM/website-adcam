47a1f974a3797d28ad22d0932241ce307d06ac44
altervayne <79285331+Altervayne@users.noreply.github.com>
2023-09-09 01:12:00 +0200

    made maintenance box lighter and added warning icon

--- src/app/_components/MaintenanceBanner.tsx
+++ src/app/_components/MaintenanceBanner.tsx
@@ -1,24 +1,37 @@
 'use client'
 /* Library Imports */
 import { makeStyles } from 'tss-react/mui'
+/* Icons Imports */
+import { PiWarningBold } from 'react-icons/pi'
 
 
 
 const useStyles = makeStyles()((theme) => {
     return {
         root: {
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+
             boxSizing: "border-box",
             padding: theme.spacing(2),
 
             borderRadius: theme.spacing(0.75),
             
-            backgroundColor: "#e64c00",
+            backgroundColor: "#ff661a",
+        },
+        icon: {
+            minHeight: "30px",
+            minWidth: "30px",
         },
         text: {
             fontWeight: 500,
             color: "white",
 
+            boxSizing: "border-box",
+
             margin: 0,
+            marginLeft: theme.spacing(2),
             padding: 0,
         },
     }
@@ -31,6 +44,7 @@ export default function MaintenanceBanner() {
 
     return (      
         <div className={ classes.root }>
+            <PiWarningBold className={ classes.icon } />
             <p className={ classes.text }>{ `Pour des raisons de maintenance, les formulaires de contact ne fonctionnent actuellement pas. Pour toute demande, joignez nous au 06 95 86 91 76.` }</p>
         </div>
     )
