e85aadcbcf8ea45f4e72c1fa5f3e84bc6f9376da
Florian Douay <flooooo85@gmail.com>
2023-09-12 01:09:56 +0200

    created email api route

---
+++ src/app/api/email/route.ts
@@ -0,0 +1,29 @@
+/* Types Imports */
+import { EmailData } from '@/app/_types/email'
+/* Libraries Imports */
+import sgMail from '@sendgrid/mail'
+import { NextRequest, NextResponse } from 'next/server'
+
+let sgApiKey = ""
+if(process.env.SENDGRID_API_KEY){
+    sgApiKey = process.env.SENDGRID_API_KEY
+}
+
+sgMail.setApiKey(sgApiKey)
+
+
+
+export async function POST(req: NextRequest) {
+    const { to, subject, text }: any  = req.body
+
+    const msg = {
+        to,
+        from: 'PLACEHOLDER',
+        subject,
+        text,
+    }
+
+    const response = await sgMail.send(msg)
+
+    return NextResponse.json({ response })
+}
--- src/app/api/reviews/route.ts
+++ src/app/api/reviews/route.ts
@@ -3,21 +3,13 @@ import { requestEndpoint } from "../../_utils/handleEnvVariables"
 /* Library Imports */
 import axios from 'axios'
 import { NextRequest, NextResponse } from "next/server"
-import { NextApiResponse } from "next"
 
 
 
-export async function GET(req: NextRequest, res: NextApiResponse) {
-    try {
+export async function GET(req: NextRequest) {
+    const response = await axios.get( requestEndpoint )
 
-        const response = await axios.get( requestEndpoint )
+    console.log('-----------Received response is-----------', response)
 
-        console.log('-----------Received response is-----------', response)
-
-        return NextResponse.json({ response })
-
-    } catch (error) {
-        console.error('Error while fetching Google reviews:', error)
-        res.status(500).json({ error: 'Unable to fetch Google reviews' })
-    }
+    return new NextResponse(JSON.stringify({ response }), {status: 200})
 }
