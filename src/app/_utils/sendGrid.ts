8fefe2fa0bcce781acf1d73587b7c3f3e41dc0f9
Florian Douay <flooooo85@gmail.com>
2023-10-13 17:45:56 +0200

    removed redundant code

--- src/app/_utils/sendGrid.ts
+++
@@ -1,9 +0,0 @@
-import sendgridMail from '@sendgrid/mail'
-
-
-
-sendgridMail.setApiKey(process.env.SENDGRID_API_KEY as string)
-
-
-
-export default sendgridMail
--- src/app/api/email/route.ts
+++ src/app/api/email/route.ts
@@ -2,23 +2,26 @@
 const destination = process.env.SENDGRID_DESTINATION
 /* Libraries Imports */
 import sgMail from '@sendgrid/mail'
-import { NextResponse } from 'next/server'
 
 
 
-if(process.env.SENDGRID_API_KEY !== undefined) {
-    sgMail.setApiKey(process.env.SENDGRID_API_KEY)
-} else {
-    console.error(`The SendGrid API key is undefined!`)
+export async function GET() {
+    return new Response('There is no server logic on GET route for email API', { status: 200 })
 }
 
 
 
 export async function POST(req: any) {
+    if(process.env.SENDGRID_API_KEY !== undefined) {
+        sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+    } else {
+        console.error(`The SendGrid API key is undefined!`)
+    }
+    
     const data = await req.json()
 
     if(!data.subject || !data.text) {
-        return new NextResponse('Error while sending email', { status: 500 })
+        return new Response('Error while sending email', { status: 500 })
     }
 
     const msg = {
@@ -34,10 +37,10 @@ export async function POST(req: any) {
         .send(msg)
         .then(() => {
             console.log('Email sent')
-            return new NextResponse('Email sent', { status: 200 })
+            return new Response('Email sent', { status: 200 })
         })
         .catch((error) => {
             console.error(error)
-            return new NextResponse('Error while sending email', { status: 500 })
+            return new Response('Error while sending email', { status: 500 })
         })
 }
